80494b085a6535a4a4cd84e6bda1800f4867692f
apriljewel <april.domingo@obf.ateneo.edu>
2015-03-04 16:09:59 +0800

    Test scenarios

---
+++ src/april.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class TestCase {
+
+	/**
+	* This test scenario is the JUnit test case for the rollDice function.
+	*/	
+
+	@Test
+	public void testRollDice() {
+		Random rand = new Random();
+		assertNotNull(rand);
+	}
+	
+	/**
+	* This test scenario is the JUnit test case for the pass function.
+	*/
+	
+	public void testPass() {
+		
+	}
+	
+	/**
+	* This test scenario is the JUnit test case for the encounter function.
+	*/
+
+	public void testEncounter() {
+		
+	}
+}
+
