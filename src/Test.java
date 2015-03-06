32252d1d6c63d73965363eb5b994f275d70f9dcd
Charles Lee <charles26lee@gmail.com>
2015-03-06 16:12:31 +0800

    Added Test file

---
+++ src/Test.java
@@ -0,0 +1,68 @@
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class TestCase {
+
+	/**
+	* This test scenario is the JUnit test case for the rollDice function.
+	*/	
+
+	@Test
+	public void testRollDice() {
+		Random rand = new Random();
+		assertNotNull(rand);
+	}
+	
+	/**
+	* This test scenario is the JUnit test case for the pass function.
+	*/
+	
+	public void testPass() {
+		
+	}
+	
+	/**
+	* This test scenario is the JUnit test case for the encounter function.
+	*/
+
+	public void testEncounter() {
+		Scanner input = new Scanner(System.in);
+		assertNotNull(input);
+	}
+	
+	/**
+	* This test scenario is the JUnit test case for the takeDamage function.
+	*/
+	
+	public void testTakeDamage() {
+		
+	}
+	
+	/**
+	* This test scenario is the JUnit test case for the heal function.
+	*/
+	
+	public void testHeal() {
+		
+	}
+	
+	/**
+	* This test scenario is the JUnit test case for the getHP function.
+	*/
+	
+	public void testGetHP() {
+		
+	}
+	
+	/**
+	* This test scenario is the JUnit test case for the isAlive function.
+	*/
+	
+	public void testIsAlive() {
+		
+	}
+}
+
